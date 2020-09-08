62c50c813c0c52f5fe4a171ec54545347e37395e
rshudde <rachael.shudde@gmail.com>
2020-09-08 12:25:01 -0500

    removing TCGA examples

--- R/DATA_documentation.R
+++
@@ -1,9 +0,0 @@
-#' OV dataset
-#'
-#' A dataset containing genetic information for patients with ovarian cancer
-#'
-#' @format A data frame with 20530 rows and 306 columns (will be transposed in TCGA_cleanData())
-#' @source https://github.com/compgenome365/TCGA-Assembler-2
-"OV"
-#> [1] "OV"
-
