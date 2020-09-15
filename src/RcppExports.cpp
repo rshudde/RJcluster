7a23fd735f17a8d8c4cd34dd7942e7fb7396bdc5
rshudde <rachael.shudde@gmail.com>
2020-09-15 16:11:32 -0500

    removing several unusable things

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -72,18 +72,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// getImportantGenes_c
-arma::colvec getImportantGenes_c(const arma::mat& X, const double cluster);
-RcppExport SEXP _RJcluster_getImportantGenes_c(SEXP XSEXP, SEXP clusterSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::mat& >::type X(XSEXP);
-    Rcpp::traits::input_parameter< const double >::type cluster(clusterSEXP);
-    rcpp_result_gen = Rcpp::wrap(getImportantGenes_c(X, cluster));
-    return rcpp_result_gen;
-END_RCPP
-}
 // tcrossprod_c
 arma::mat tcrossprod_c(const arma::mat& X, const arma::mat& Y);
 RcppExport SEXP _RJcluster_tcrossprod_c(SEXP XSEXP, SEXP YSEXP) {
@@ -106,27 +94,6 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< bool >::type medians(mediansSEXP);
     rcpp_result_gen = Rcpp::wrap(scale_c(X, medians));
     return rcpp_result_gen;
-END_RCPP
-}
-// get_percentages_c
-arma::vec get_percentages_c(const arma::mat& X);
-RcppExport SEXP _RJcluster_get_percentages_c(SEXP XSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::mat& >::type X(XSEXP);
-    rcpp_result_gen = Rcpp::wrap(get_percentages_c(X));
-    return rcpp_result_gen;
-END_RCPP
-}
-// replace_zeroes_c
-void replace_zeroes_c(arma::mat& X);
-RcppExport SEXP _RJcluster_replace_zeroes_c(SEXP XSEXP) {
-BEGIN_RCPP
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::mat& >::type X(XSEXP);
-    replace_zeroes_c(X);
-    return R_NilValue;
 END_RCPP
 }
 // rcpparma_hello_world
@@ -179,11 +146,8 @@ static const R_CallMethodDef CallEntries[] = {
     {"_RJcluster_getMatrixMeans_c", (DL_FUNC) &_RJcluster_getMatrixMeans_c, 3},
     {"_RJcluster_assignGroups_c", (DL_FUNC) &_RJcluster_assignGroups_c, 4},
     {"_RJcluster_getFinalMeans_c", (DL_FUNC) &_RJcluster_getFinalMeans_c, 3},
-    {"_RJcluster_getImportantGenes_c", (DL_FUNC) &_RJcluster_getImportantGenes_c, 2},
     {"_RJcluster_tcrossprod_c", (DL_FUNC) &_RJcluster_tcrossprod_c, 2},
     {"_RJcluster_scale_c", (DL_FUNC) &_RJcluster_scale_c, 2},
-    {"_RJcluster_get_percentages_c", (DL_FUNC) &_RJcluster_get_percentages_c, 1},
-    {"_RJcluster_replace_zeroes_c", (DL_FUNC) &_RJcluster_replace_zeroes_c, 1},
     {"_RJcluster_rcpparma_hello_world", (DL_FUNC) &_RJcluster_rcpparma_hello_world, 0},
     {"_RJcluster_rcpparma_outerproduct", (DL_FUNC) &_RJcluster_rcpparma_outerproduct, 1},
     {"_RJcluster_rcpparma_innerproduct", (DL_FUNC) &_RJcluster_rcpparma_innerproduct, 1},
